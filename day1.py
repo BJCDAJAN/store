3b20463dd08b87d020f61a4fd87d04897bac5f33
BJCDAJAN <80331684+BJCDAJAN@users.noreply.github.com>
2021-03-11 17:44:46 +0800

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,45 @@
+#
+id1 = 1
+name1 = ("半袖")
+price1 = 50
+quality1 = "A+"
+type1 = ("上衣")
+sale1 = 500
+
+#
+id2 = 2
+name2 = ("长裤")
+price2 = 75
+quality2 = "B+"
+type2 = ("裤子")
+sale2 = 600
+
+#
+id3 = 3
+name3 = ("外套")
+price3 = 150
+quality3 = "A"
+type3 = ("上衣")
+sale3 = 550
+
+
+print("---------------------------------")
+print("-         jason衣服商城          -")
+print("---------------------------------")
+print("编号  衣服名称  价格  品质  类型  销量")
+print("---------------------------------")
+print(id1,"  ",name1,"    ",price1," ",quality1," ",type1," ",sale1)
+print(id2,"  ",name2,"    ",price2," ",quality2," ",type2," ",sale2)
+print(id3,"  ",name3,"    ",price3,"",quality3,"  ",type3," ",sale3)
+
+
+
+
+
+
+
+
+
+
+
+
